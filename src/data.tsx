05924b7502aaecff48eef5bc57772373dd352da5
Zaki Machfj <zakimachfj@gmail.com>
2022-03-14 18:17:07 -0400

    add data for experience

--- src/data.tsx
+++ src/data.tsx
@@ -7,10 +7,13 @@ export const navData: NavLink[] = [
     { id: 'projects', value: 'Projects', key: 2 },
 ];
 
-export const homeData: LandData = {
+export const homeData = {
     heading: 'Zaki Machfj',
     position: 'Software Developer',
-    company: 'Dejero Labs',
+    company: {
+        name: 'Dejero Labs',
+        website: 'https://www.dejero.com',
+    },
     tags: [
         { value: 'Developer', id: 0 },
         { value: 'Student', id: 1 },
@@ -27,3 +30,26 @@ export const homeData: LandData = {
         },
     ],
 };
+
+export const experienceData = [
+    {
+        company: {
+            name: 'Quill Podcasting',
+            website: 'https://www.quillpodcasting.com',
+            logo: 'https://uploads-ssl.webflow.com/5d6af44f068031b45b8e83a5/5dd68aa836a0d4abf9966376_White%20on%20Purple.png',
+        },
+        position: 'Software Developer',
+        tenure: 'May - Aug. 2021',
+        location: 'Toronto, ON',
+        tools: ['Alpine.js', 'Tailwindcss', 'PostgreSQL', 'Ruby on Rails', 'AWS EC2'],
+        body: (
+            <p>
+                This was my first internship, and it was one of the most fun and valuable experiences I've had. The
+                path to this role was not easy; I applied to around 300 jobs on WaterlooWorks and got around 3
+                interviews, and this was my only offer. It was my first job search and I was extremely nervous, but
+                I learned a lot, especially about the importance of treating an interview like a conversation
+                rather than, well, an interview.
+            </p>
+        ),
+    },
+];
